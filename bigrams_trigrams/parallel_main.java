2c014ab3844c09e9d30b2dc7ac634e41f4d7b492
Armand Palla <65510771+Armand-blip@users.noreply.github.com>
2021-03-05 16:19:00 +0100

    Add files via upload

---
+++ bigrams_trigrams/parallel_main.java
@@ -0,0 +1,114 @@
+package bigrams_trigrams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class parallel_main {
+	
+	// Merge n_grams in finalNgrams
+    public static ConcurrentHashMap<String, Integer> HashMerge(ConcurrentHashMap<String, Integer> n_grams, ConcurrentHashMap<String, Integer> finalNgrams) {
+        for (ConcurrentHashMap.Entry<String, Integer> entry : n_grams.entrySet()) {
+            int newValue = entry.getValue();
+            Integer existingValue = finalNgrams.get(entry.getKey());
+            if (existingValue != null) {
+                newValue = newValue + existingValue;
+            }
+            finalNgrams.put(entry.getKey(), newValue);
+        }
+       
+        return finalNgrams;
+    }
+    // Read from the text and remove special characters ( ();:,.& ).
+    public static char[] readTextFromFile() {
+        Path path = Paths.get("/Users/Armando/eclipse-workspace/bigrams_trigrams/src/1-text50KB.txt");
+
+        try {
+            Stream<String> lines = Files.lines(path);
+            char[] filestring = (lines.collect(Collectors.joining())).replaceAll("[ '&();:,.]", "").toCharArray();
+
+            for(int i = 0; i < filestring.length - 1; ++i) {
+                if (Character.isUpperCase(filestring[i])) {
+                    filestring[i] = Character.toLowerCase(filestring[i]);
+                }
+            }
+
+            return filestring;
+
+        }
+
+        catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        char[] fileString = readTextFromFile();
+
+        int fileLen = fileString.length;
+        //int threads = Runtime.getRuntime().availableProcessors();
+        int realThreads = 2;
+
+        ConcurrentHashMap<String, Integer> finalNgrams = new ConcurrentHashMap(); // Create ConcurrentHashMap containing <gram, value>
+
+        ArrayList<Future> futuresArray = new ArrayList<>(); // Create an ArrayList of Future that will contain the threads results
+
+        ExecutorService executor = Executors.newFixedThreadPool(realThreads); // create the threads
+
+        long start, end;
+        start = System.currentTimeMillis();
+
+        int n = 3; //n grams
+        double k = Math.floor(fileLen/realThreads);// divide text according to number of threads
+
+        for (int i = 0; i < realThreads; i++) {
+
+            Future f = executor.submit(new parallel_thread("t" + i, i * k, ((i+1) * k) + (n - 1) - 1, n, fileString));//executor.submit start the Callable call that save the results in a Future
+            futuresArray.add(f);
+
+        }
+
+        try{
+            for (Future <ConcurrentHashMap<String, Integer>> f : futuresArray) {
+                ConcurrentHashMap<String, Integer> n_grams = f.get(); // f.get return the thread result
+                HashMerge(n_grams,finalNgrams);
+            }
+            System.out.println(finalNgrams);
+            awaitTerminationAfterShutdown(executor);
+
+            end = System.currentTimeMillis();
+
+            System.out.println(n + "-grams calculated in " + (end - start) + " ms");
+
+        }
+
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }//public static void main
+
+    public static void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+}//public class parallel_main
---
+++ bigrams_trigrams/sequential_version.java
@@ -0,0 +1,79 @@
+package bigrams_trigrams;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Map.Entry;
+public class sequential_version {
+
+	public static char[] readText() {
+        Path path = Paths.get("/Users/Armando/eclipse-workspace/bigrams_trigrams/src/1-text50KB.txt");
+
+        try {
+        	//Read all rows from a file as a Stream. 
+            Stream<String> rows = Files.lines(path);
+            
+            // Returns a Collector that concatenates the input elements into a String and converts this string to a new character array.
+            char[] file = (rows.collect(Collectors.joining())).replaceAll("[ ;:&'(),.]", "").toCharArray();
+
+            for(int i = 0; i < file.length - 1; ++i) {
+                if (Character.isUpperCase(file[i])) {
+                    file[i] = Character.toLowerCase(file[i]);
+                }
+            }
+
+            return file;
+
+        }
+
+        catch (IOException e) {
+            System.out.println(e);
+            System.exit(1); //bad command-line, can't find file, unsuccessful termination.
+            return null;
+        }
+    } // public static char[]
+	
+	 public static HashMap<String, Integer> ngrams(int n, char[] file) {
+        HashMap<String, Integer> hashMap = new HashMap();
+
+        for(int i = 0; i < file.length - n + 1; ++i) {
+        	
+        	//A string buffer is like a String, but can be modified.String buffers are safe for use by multiple threads.
+            StringBuffer buffer = new StringBuffer();
+            //StringBuilder builder = new StringBuilder();
+            for(int j = 0; j < n; ++j) {
+                buffer.append(file[i + j]);
+            }
+
+            String key = buffer.toString();
+            //Control if the key is added one time in the hashMap
+            if (!hashMap.containsKey(key)) {
+                hashMap.put(key, 1);
+            }
+            //if it is added before increment the value with one
+            else if (hashMap.containsKey(key)) {
+                hashMap.put(key, hashMap.get(key) + 1);
+            }
+        }
+
+        return hashMap;
+        
+    }//public static HashMap <String, Integer> ngrams
+	 public static void main(String[] args) {
+
+         char[] file = readText();
+         long start, end;
+
+         start = System.currentTimeMillis();
+         HashMap hmap = ngrams(3, file);
+         end = System.currentTimeMillis();
+         
+         System.out.println("Time in milliseconds is: " + (end - start) + " milliseconds." );
+         System.out.println("Elements of the HashMap are: " + hmap);
+ } //public static void main
+} // public class sequential_version
